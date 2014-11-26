af1295fb64d51e0f7feefaeef405e920e064b6d4
Allwin Panikulam <allwinpb@gmail.com>
2014-11-26 17:32:09 +0800

    Bugfixes

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ watcher
 
 var htmlParser = new htmlparser2.Parser({
 	onopentag: function(name, attribs){
-		if(name === "script" && (attribs.src != "" || attribs.src != "#")){
+		if(name === "script" && isValid(attribs.src)){
 			addDependency(attribs.src, currFile)
-		}else if(name === "link" && (attribs.rel != "" || attribs.rel != "#")){
-			addDependency(attribs.rel, currFile)
+		}else if(name === "link" && isValid(attribs.href)){
+			addDependency(attribs.href, currFile)
 		}
 	}
 });
@@ -47,22 +47,37 @@ function updateDepMap(path, filename){
 
 function addDependency(source, dependency){
 	source = normalizePath(source)
-	if(key in dependencyMap){
-		dependencyMap[key][value] = true
+	if(source in dependencyMap){
+		dependencyMap[source][value] = true
 	}else{
-		dependencyMap[key] = {}
+		dependencyMap[source] = {}
 	}
 }
 
 function normalizePath(path){
-	if(path.indexOf("http") == 0){
+	if(path.indexOf("http") == 0 || path.indexOf("//") == 0){
 		//Absolute path, ignore for now
 		return null
 	}else if(path.indexOf("/") == 0){
 		//Relative to the root directory
-		return path.join(rootDir, path)
+		return pathParser.join(rootDir, path)
 	}else{
 		//Relative to the containing HTML file
-		return path.join(currFilePath, path)
+		return pathParser.join(currFilePath, path)
 	}
+}
+
+function isValid(src){
+	return !isEqualsAny(src, [undefined, "", "#"])
+}
+
+function isEqualsAny(input, args_array){
+	var output = false
+	for(var i=0; i < args_array.length; i++){
+		output = output || (input === args_array[i])
+		if(output){
+			break;
+		}
+	}
+	return output
 }
